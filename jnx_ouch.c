4c9d5c945fafdca962b07dd2373a0a27710f8d70
Merdan Atajanov <merdan.atajanov@japannext.co.jp>
2017-05-26 08:33:49 +0900

    Chase upstream changes, remove deprecated tvb_length call.
    Followup on be6fb606915f4ccc68a62195619d176cd62f79f9

--- jnx_ouch.c
+++ jnx_ouch.c
@@ -579,7 +579,7 @@ dissect_jnx_ouch(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data
         break;
     }
 
-    return tvb_length(tvb);
+    return tvb_captured_length(tvb);
 }
 
 /* Register the protocol with Wireshark */

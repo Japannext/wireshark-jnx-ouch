3ee0a4849604aab7d8cf0d598b7c0b3fbfb73681
Georgi Georgiev <georgi.georgiev@japannext.co.jp>
2021-08-24 13:13:30 +0900

    Clean up a redundant variable assignment.

--- build.sh
+++ build.sh
@@ -13,9 +13,8 @@ fi
 if [[ -z $version_release ]]; then
 	echo "You have a buggy version of wireshark-devel which does not define the VERSION_RELEASE variable"
 	if type -p rpm >/dev/null; then
-		version_release=$(rpm -q wireshark-devel --qf %{VERSION} | cut -d. -f1-2)
-		echo "Trying to work around it by assuming VERSION_RELEASE=${version_release}"
-		VERSION_RELEASE=${version_release}
+		VERSION_RELEASE=$(rpm -q wireshark-devel --qf %{VERSION} | cut -d. -f1-2)
+		echo "Trying to work around it by assuming VERSION_RELEASE=${VERSION_RELEASE}"
 	else
 		echo "The compiled plugin will most likely refuse to be loaded by wireshark, but buildig it anyway"
 	fi
